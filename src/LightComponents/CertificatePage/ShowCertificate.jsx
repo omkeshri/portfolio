1cdc036794b96a3f1c7a1fb9fd0e71cda068131e
OmKeshri <omkeshri21@gmail.com>
2024-12-12 17:32:45 +0530

    Solved minor bug

--- src/LightComponents/CertificatePage/ShowCertificate.jsx
+++ src/LightComponents/CertificatePage/ShowCertificate.jsx
@@ -8,6 +8,7 @@ import genai from "../../Images/genai.png";
 import llm from "../../Images/llm.png";
 import genaillm from "../../Images/genaillm.png";
 import network from "../../Images/network.png";
+import nptelCloud from "../../Images/nptelCloud.png";
 import { motion } from "framer-motion";
 
 const ShowCertificate = ({ name, showItems, setShowIndex, imgSrc }) => {
@@ -17,6 +18,7 @@ const ShowCertificate = ({ name, showItems, setShowIndex, imgSrc }) => {
 
   const images = {
     gfg,
+    nptelCloud,
     dsa,
     dp,
     frontend,
@@ -28,8 +30,7 @@ const ShowCertificate = ({ name, showItems, setShowIndex, imgSrc }) => {
   };
 
   return (
-    <div className="w-full flex items-center flex-col"
-    >
+    <div className="w-full flex items-center flex-col">
       <div
         className="w-full flex justify-between cursor-pointer bg-[#f9fafb] shadow-md text-black py-[0.65rem] px-4 rounded-lg font-medium"
         onClick={handleShowCertificate}
@@ -39,10 +40,10 @@ const ShowCertificate = ({ name, showItems, setShowIndex, imgSrc }) => {
       </div>
       {showItems && (
         <motion.img
-          className="w-[98%] xl:h-[40rem] rounded-b-lg -rounded-t-lg"
+          className="w-[98%] xl:h-[40rem] rounded-b-lg -rounded-t-lg border border-t-0 border-gray-400 rounded-md shadow-md"
           src={images[imgSrc]}
-          initial={{ opacity: 0, y:'-100vh' }}
-          animate={{ opacity: 1, y:0 }}
+          initial={{ opacity: 0, y: "-100vh" }}
+          animate={{ opacity: 1, y: 0 }}
         ></motion.img>
       )}
     </div>
